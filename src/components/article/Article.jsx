4b947d408c94485e610e7401184a7441e8101cad
Zaid Ahmad <zaidahmad9876@gmail.com>
2021-12-29 21:26:21 +0500

    Added blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,21 @@
-import './article.css'
+import "./article.css";
 
-const Article = () => {
-    return (
-        <div>
-            Article
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blogImg" />
+      </div>
+
+      <div className="gpt3__blog-container_article-content">
+        <div className="gpt3__blog-container_article-content_date">
+          <p>{date}</p>
+          <h3>{title}</h3>
         </div>
-    )
-}
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
 
-export default Article
+export default Article;
